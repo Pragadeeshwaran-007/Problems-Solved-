47a1ac5c24ee3c0a364fdd13f0a9f7074e8888d2
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-11-11 06:32:50 +0530

    Add files via upload

---
+++ leetcode 1092.cpp
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Solution {
+
+    public String shortestCommonSupersequence(String s1, String s2) {
+        char[] lcs = lcs(s1, s2).toCharArray();
+        int i = 0, j = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : lcs) {
+            while (s1.charAt(i) != c) sb.append(s1.charAt(i++));
+            while (s2.charAt(j) != c) sb.append(s2.charAt(j++));
+            sb.append(c);
+            i++;
+            j++;
+        }
+
+        sb.append(s1.substring(i)).append(s2.substring(j));
+        return sb.toString();
+    }
+
+    private String lcs(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) i--;
+            else j--;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    // Optional main method for testing
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        System.out.println(new Solution().shortestCommonSupersequence(s1, s2));
+        sc.close();
+    }
+}
