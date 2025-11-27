fab982ec5b0a51c6b6b14f596cb31dd892222481
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-11-27 15:54:11 +0530

    Add files via upload

---
+++ leetcode 101.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return isValid(root->left, root->right);
+    }
+
+    bool isValid(TreeNode* n1, TreeNode* n2) {
+        if (n1 == nullptr && n2 == nullptr) return true;
+        if (n1 == nullptr || n2 == nullptr) return false;
+
+        return n1->val == n2->val &&
+               isValid(n1->left, n2->right) &&
+               isValid(n1->right, n2->left);
+    }
+};
